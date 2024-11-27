f860faeb2a25b9b75e1738eede470c237fefcca0
FlorHeredia <florheredia95@hotmail.es>
2024-11-27 20:27:11 -0300

    Borre codigo comentado

---
+++ paciente.ts
@@ -0,0 +1,84 @@
+
+
+  /*Pacientes (mascotas): nombre, especie (si no es perro o gato, deberá registrarse como exótica), 
+  id del dueño, las veterinarias deben contar con la opción de alta, baja y modificación de los mismos. */
+
+
+   // Clase Paciente
+   export class Paciente{
+    private nombre: string;
+    private especie: string;
+    public id: number; 
+
+    //constructor
+
+    public constructor(nombre: string, especie: string,idDueño: number){
+        this.nombre = nombre;
+        this.especie = especie;
+        this.id = idDueño;
+    }
+
+    //getters
+    public getNombre():string{
+        return this.nombre
+    }
+    
+    public getEspecie():string{
+        return this.especie
+    }
+
+   public getId():number{
+        return this.id
+    }
+    
+    
+    
+
+    //setters
+    public setEspecie(Especie:string):void{
+        this.especie=Especie
+    }
+
+    public setNombre(nuevoNombre:string):void{
+        this.nombre=nuevoNombre
+    }
+
+
+    // mascota exotica
+    public esExotica():boolean{
+        let ok:boolean = false;
+        if (this.especie != "perro" && this.especie != "gato"){
+            ok = true;
+            console.log("el paciente " + this.nombre + " es una especie exotica")
+        }else {
+            console.log("el paciente " + this.nombre + " no es una especie exotica")
+        }
+        return ok
+    }
+
+    public datosAnimal(): string {
+        return `paciente: Nombre: ${this.nombre}, Especie: ${this.especie}, Dueño: ${this.id}`;
+      }
+
+      //modificar paciente
+    
+      public modificarPaciente(  nombreNuevo: string, especieNuevo: string): void {
+        let pacienteAModificar: Paciente| undefined;//me lo agrega
+       
+        if (pacienteAModificar) {
+            pacienteAModificar.nombre = nombreNuevo;
+            pacienteAModificar.especie = especieNuevo;
+          console.log(`Paciente modificado: ${pacienteAModificar.datosAnimal()}`);
+        } else {
+          console.log(" El paciente no está registrado");
+        }
+      }
+}
+/*
+public getCostoTotal(): number {
+  let precio = this.getPrecioBase();
+  if (this.limpieza) {
+      precio *= 1.10; // 10% adicional de limpieza
+  }
+  return precio * this.iva; // Se agrega 21% de IVA
+}*/
--- redVeterinaria.ts
+++ redVeterinaria.ts
@@ -1,58 +1,151 @@
-// Nuestro cliente es una red de veterinarias y desea
-// poder acceder a la siguiente información:
-// ● Sucursales de Veterinarias
-// ● Clientes
-// ● Pacientes (mascotas)
-// ● Proveedores
+import { Cliente } from "./cliente"
+import { Paciente } from "./paciente";
+import { Proveedor } from "./proveedor";
+import { Veterinaria } from "./sucursalVeterinaria";
+import { existeId, crearNumRandom} from "./sucursalVeterinaria";
+import * as readlineSync from 'readline-sync';
 
-export class redVeterinaria {
-    nombre: string;
-    direccion: string;
-    ID: number;
+/*Nuestro cliente es una red de veterinarias y desea poder acceder a la siguiente información:
+Sucursales de Veterinarias, Clientes,Pacientes (mascotas) yProveedores 
+
+Proveedores: la red debe contar con la opción de alta, baja y modificación de los mismos. 
+
+Veterinarias: La red debe tener la posibilidad de dar de alta, modificar datos o dar de baja las mismas.
+
+IMPORTANTE: los id deben ser únicos, por lo tanto al generarse, antes de guardarlos se debe validar que no exista. 
+Si ya existe se debe volver a generar.*/
+
+
+  //Clase RedVeterinarias
+
+  export  class RedVeterinaria{
+
+    private veterinarias: Veterinaria [];
+    private proveedores:  Proveedor [];
+
+
+//constructor
+    public constructor(){
+        this.veterinarias =[];
+        this.proveedores = [];
+    }
+
+    //getters
+    public getVeterinarias() :Veterinaria []{
+        return this.veterinarias;
+    }
     
-
-    constructor(nombre:string, direccion:string, id:number){
-        this.nombre=nombre;
-        this.direccion=direccion;
-        this.ID=id;
-
+    public getProveedores() : Proveedor []{
+        return this.proveedores;
     }
 
-    //metodos getters
+  
+    //setters
+    public setVeterinarias(veterinarias: Veterinaria[]) : void {
+        this.veterinarias = veterinarias;
 
-    getNombre(){
-        return this.nombre;
     }
-    getDireccion(){
-        return this.direccion;
-    }
-    getID(){
-        return this.ID;
-    }   
-
-    //metodos setters
-    setNombre(nombre:string){
-        this.nombre=nombre;
-    }
-    setDireccion(direccion:string){
-        this.direccion=direccion;
-    }
-    setID(id:number){
-        this.ID=id;
-    } 
     
-    //otros metodos
-    darDeAlta(){
-        console.log("Se ha dado de alta la sucursal "+this.nombre);
+    public setProveedores(proveedores: Proveedor []) {
+        this.proveedores = proveedores;
+    
+  }
+
+  }
+
+//--------------------------------------- PROVEEDORES
+
+
+//Función para Alta  Proveedor
+
+export function altaProveedor(arrProveedor: Proveedor[]){
+    let nombre: string = readlineSync.question("Ingrese Apellido y Nombre del proveedor: ");
+    let telefono: number = readlineSync.questionInt("Ingrese el n° de telefono del proveedor: ");
+      
+    let id: number = crearNumRandom(20000);
+    while(existeId(arrProveedor,id)==true){
+      id=crearNumRandom(20000);
     }
-    darDeBaja(){
-        console.log("Se ha dado de baja la sucursal "+this.nombre);
-    }   
-    modificarDatos(nombre:string, direccion:string){      // consultar si al tener este metodo deberiamos sacar los setters de nombre y direccion
-        this.nombre=nombre;
-        this.direccion=direccion;
-        console.log("Datos modificados");
-        console.log("Nuevo nombre: "+this.nombre);
-        console.log("Nueva direccion: "+this.direccion);
+  
+    let nProveedor: Proveedor = new Proveedor(nombre, telefono, id);
+    arrProveedor.push(nProveedor);
+    console.log(arrProveedor);
+  }
+  
+  
+  //Función para modificar datos de Proveedor
+  export function modificarProveedor(arregloProveedores: Proveedor[], posicion: number){
+    let nombre: string = readlineSync.question("Ingrese el nombre modificado: ");
+    let telefono: number = readlineSync.questionInt("Ingrese el numero nuevo: ");
+    let id: number = arregloProveedores[posicion].getId()
+    
+    let proveedorMod: Proveedor = new Proveedor(nombre, telefono, id)
+    delete arregloProveedores[posicion]
+    arregloProveedores[posicion] = proveedorMod;
+    console.log(arregloProveedores)
+  }
+  
+  //Función para dar de baja Proveedor 
+  
+  export function bajaProveedor(proveedor: Proveedor[]){ 
+    let bajaId:number=readlineSync.questionInt("Ingrese Id a dar de baja: ")
+    for (let i= 0; i< proveedor.length; i++){
+      if (bajaId === proveedor[i].getId()){
+        proveedor.splice(i,1)
+        console.log("Se dio de baja el Proveedor con id ingresado")
+      }
     }
-}
+    console.log(proveedor)
+  }
+  
+  //----------------------------------------sucursales de Veterinarias
+  
+  
+  
+  //alta Veterinaria
+  
+  export function altaVeterinaria(arrVeterinaria: Veterinaria[], arrClientes: Cliente[], arrPacientes: Paciente[]){
+      let nombre : string = readlineSync.question("Ingrese el nombre de la veterinaria: ");
+      let direccion: string = readlineSync.question("ingrese dirección: ")
+      let id: number = crearNumRandom(20000);
+      
+    while(existeId(arrVeterinaria,id)==true){
+      id=crearNumRandom(20000);
+    }
+      
+      let listaClientes: Cliente[] = arrClientes;
+      let listaGeneralPacientes: Paciente[]= arrPacientes;
+  
+    let nuevaVeterinaria: Veterinaria = new Veterinaria(nombre, direccion, id, listaClientes, listaGeneralPacientes);
+    arrVeterinaria.push(nuevaVeterinaria)
+      console.log(arrVeterinaria)
+  
+  }
+  
+  //Modificar veterinaria
+  
+  export function modificarVeterinaria(arregloVete: Veterinaria[], posicion: number, arrClientes: Array<Cliente>, arrPacientes: Array<Paciente>){
+      let nombre : string = readlineSync.question("Ingrese el nombre nuevo: ");
+      let direccion: string = readlineSync.question("ingrese nueva dirección: ");
+  
+    let id: number = arregloVete[posicion].getId()    
+  
+      let veterinariaModificada : Veterinaria = new Veterinaria (nombre, direccion, id, arrClientes, arrPacientes);
+      arregloVete[posicion] = veterinariaModificada;
+      console.log(arregloVete)
+  
+  }
+  
+  
+  //Funcion baja Veterinaria  
+  
+  export function bajaVeterinaria(arregloVete:Veterinaria[], id: number):void{
+  
+    for (let i= 0; i< arregloVete.length; i++){
+      if (id === arregloVete[i].getId()){
+        arregloVete.splice(i,1)
+      }
+    }
+    console.log(arregloVete)
+  }
+  
